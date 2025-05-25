28066ef90950073a9d96ae35ac5fb1af47802889
TagiaHuy <146831650+TagiaHuy@users.noreply.github.com>
2025-05-25 22:09:42 +0700

    say hello

--- BackEnd/src/routes/api.js
+++ BackEnd/src/routes/api.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controller');
-
+const sayhello = require('../controllers/sayHello');
 // Define API routes
 router.get('/json', controller.getData);
-
+router.get('/sayHello', sayhello.getData);
 module.exports = router;
