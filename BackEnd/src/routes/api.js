296157e2e2c52ec1f54ea3393febb0fc6f78983f
TagiaHuy <146831650+TagiaHuy@users.noreply.github.com>
2025-06-05 09:40:38 +0700

    them task

--- BackEnd/src/routes/api.js
+++ BackEnd/src/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const taskController = require('../controllers/taskController');
 
 const { authenticateToken, requireEmailVerified } = require('../middleware/auth');
 
@@ -15,5 +16,13 @@ router.post('/auth/reset-password', userController.resetPassword);
 // Protected user routes
 router.get('/user/profile', authenticateToken, userController.getProfile);
 router.put('/user/profile', authenticateToken, userController.updateProfile);
+
+// new task
+router.post('/tasks', taskController.createTask);
+// router.get('/tasks', taskController.getTasks);
+// router.get('/tasks/:id', taskController.getTaskById);
+// router.put('/tasks/:id', taskController.updateTask);
+// router.delete('/tasks/:id', taskController.deleteTask);
+
 
 module.exports = router;
